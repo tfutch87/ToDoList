9910f2235fb718d0c71486d8334e1b3bb52db477
Tyren Futch <tfutch43.tf@gmail.com>
2022-08-04 21:23:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var PORT = 3000;
+
+app.use(express.static(__dirname + '/public'));
+ 
+// Without middleware
+app.get('/', function(req, res){
+    var options = {
+        root: path.join(__dirname)
+    };
+     
+    var fileName = 'index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
+ 
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
